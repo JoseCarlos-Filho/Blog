3a08372508cd51173a8ecc4fe41961dc6bff9122
José Carlos <etijcvivi@gmail.com>
2022-11-23 22:11:19 -0300

    Projeto Blog - Configuração das rotas

--- blog-react-axios/src/main.jsx
+++ blog-react-axios/src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// import das bibliotecas para criação das Rotas
+import { createrBrowserRoute, RouterProvider, Route } from "react-router-dom";
+
+import "./index.css";
+
+// Array de Objetos com a configuração das rotas
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+      },
+      {
+        path: "/new",
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
