062caaa01c92d1f2f0efbaeacf3f4fe71fd912f2
José Carlos <etijcvivi@gmail.com>
2022-12-19 22:07:13 -0300

    Projeto Blog React+Vite+Axios: useState, useEffect, conexão com a API fazendo um get.

--- blog-react-axios/src/routes/Home.jsx
+++ blog-react-axios/src/routes/Home.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState, useEffect } from "react";
 
 import { Link } from "react-router-dom";
@@ -6,7 +7,20 @@ import "../routes/Home.css";
 const Home = () => {
   const [posts, setPosts] = useState([]);
 
-  const getPosts = async () => {};
+  const getPosts = async () => {
+    // console.log("testando");
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+
+      console.log(response);
+    } catch (e) {}
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
   return <div>Home</div>;
 };
 
