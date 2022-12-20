4132b1dce25310de4913062954cda335ddb03ade
José Carlos <etijcvivi@gmail.com>
2022-12-19 21:54:21 -0300

    Projeto Blog React+Vite+Axios: adicionando estilos e elementos na page home.jsx

--- blog-react-axios/src/routes/Home.jsx
+++ blog-react-axios/src/routes/Home.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
+import { Link } from "react-router-dom";
+
+import "../routes/Home.css";
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {};
   return <div>Home</div>;
 };
 
